95930a980aa8954bb29d8691063ec181d4e4d9bb
alyanrajpoot605 <alyanrajpoot605@gmail.com>
2026-07-10 21:52:24 +0500

    Add Mega Mart project

---
+++ mega Mart.py
@@ -0,0 +1,530 @@
+print("==========MEGA MART==========")
+
+print("                              ")
+print("=========FOOD SECTION==========")
+print("                                ")
+
+rice = int(input("enter the rice price="))
+milk = int(input("enter the milk price="))
+sugar = int(input("enter the sugar price="))
+salt = int(input("enter the salt price="))
+flour = int(input("enter the flour price="))
+lentils = int(input("enter the lentils price="))
+vegetables = int(input("enter the vegetables price="))
+eggs = int(input("enter the eggs price="))
+meat = int(input("enter the meat price="))
+cooking_oil = int(input("enter the cooking oil price="))
+spices = int(input("enter the spices="))
+gram_flour = int(input("enter the gram flour price="))
+yogurt = int(input("enter the yogurt price="))
+butter = int(input("enter the butter price="))
+cottage_cheese = int(input("enter the cottage cheese price="))
+meve = int(input("enter the meve price="))
+all_purpose_flour = int(input("enter the all-purpose flour price="))
+ginger_and_garlic = int(input("enter the ginger and garlic price="))
+honey = int(input("enter the honey price="))
+fresh_herbs = int(input("enter the frash herbs price="))
+semolina = int(input("enter the semolina price="))
+broken_wheat = int(input("enter the broken wheat price="))
+tapioca_pearls = int(input("enter the tapioca pearls price="))
+cornmeal = int(input("enter the cornmeal price="))
+chickpeas = int(input("enter the chickpeas price="))
+vinegar = int(input("enter the vinegar price="))
+pickle = int(input("enter the pickle price="))
+print("                             ")
+
+print("====================")
+print("==========FOOD SECTION BILL==========")
+print("                                     ")
+food_section_bills = (rice
++milk
++sugar
++flour
++lentils
++salt
++vegetables
++eggs
++meat
++cooking_oil
++spices
++gram_flour
++yogurt
++butter
++cottage_cheese
++meve
++all_purpose_flour
++ginger_and_garlic
++honey
++fresh_herbs
++semolina
++broken_wheat
++tapioca_pearls
++cornmeal
++chickpeas
++vinegar
++pickle
+)
+print("======================")
+print("==========TOTAL FOOD SECTION BILL==========")
+print("                                             ")
+print("FOOD SECTION BILL=", food_section_bills)
+print("====================")
+
+print("                                               ")
+print("==========ELECTRIC SECTION==========")
+print("                                     ")
+Electric_kattle = int(input("enter the electric kattle price="))
+Blender = int(input("enter the blender price="))
+Iron= int(input("enter the iron price="))
+Hair_Dryer = int(input("enter the hair dryer price="))
+Hand_Blender = int(input("enter the hand blender price="))
+Toaster = int(input("enter the toaster price="))
+Sandwich_Maker = int(input("enter the sandwich maker price="))
+Extension_Lead = int(input("enter the extension lead price="))
+LED_Bulb = int(input("enter ths led bulb price="))
+Wall_Clock = int(input("enter the wall clock price="))
+Desk_Fan = int(input("enter the desk fan price="))
+Hair_Straightener = int(input("enter the hair straightener price="))
+Power_Bank = int(input("enter the power bank price="))
+USB_Cable = int(input("enter the usb cable price="))
+Headphones = int(input("enter the headphones price="))
+Torch_Emergency_Light = int(input("enter the torch/emergench light price="))
+Weighing_Scale = int(input("enter the weighing scale price="))
+Rice_Cooker = int(input("enter the rice cooker price="))
+Microwave_Oven = int(input("enter the microwave oven price="))
+Air_Cooler = int(input("enter the air cooler price="))
+print("                                             ")
+print("====================")
+print("                                           ")
+print("==========ELECTRIC SECTION BILL==========")
+print("                                             ")
+
+electric_section_bill = (Electric_kattle
++Blender
++Iron
++Hair_Dryer
++Hand_Blender
++Toaster
++Sandwich_Maker
++Extension_Lead
++LED_Bulb
++Wall_Clock
++Desk_Fan
++Hair_Straightener
++Power_Bank
++USB_Cable
++Headphones
++Torch_Emergency_Light
++Weighing_Scale
++Rice_Cooker
++Microwave_Oven
++Air_Cooler
+)
+print("======================")
+print("========== TOTAL ELECTRIC SECTION BILL==========")
+print("                                     ")
+print("ELECTEIC SECTIONBILL", electric_section_bill)
+print("====================")
+
+print("                                 ")
+print("===========HOME SECTION==========")
+print("                                  ")
+Non_stick_Pan = int(input("enter the non stick pan price="))
+Pressure_cooker = int(input("enter the pressure cooker price="))
+Dinner_Set = int(input("enter the dinner set price="))
+Glass_Tumblers = int(input("enter the glass tumlers price="))
+Stainless_Steel_Spoons = int(input("enter the stainless steel price="))
+Plastic_Storage_Containers = int(input("enter the plastic storage containers price="))
+Vegetable_Chopper = int(input("enter the vegetable chopper price="))
+Cleaning_Mop = int (input("enter the cleaning mop price="))
+Dustbin = int(input("enter the dusrbin price="))
+Disbwashing_Brush= int(input("enter the dishwashing brush price="))
+Laundry_Basket = int(input("enter the laundry basket price="))
+lroning_Board = int(input("enter the ironing board price="))
+Curtains = int(input("enter the curtains price="))
+Table_Mats = int(input("enter the table mats price="))
+Door_Mats = int(input("enter the door mats price="))
+Hangers = int(input("enter the hangers price="))
+Kitchen_Apron = int(input("enter the kitchen aprone price="))
+Knife_Set = int(input("enter the knife set price="))
+Serving_Trays = int(input("enter the serving trays price="))
+Water_Bottle = int(input("enter the water bottle price="))
+print("                                              ")
+
+print("====================")
+print("==========HOME SECTION BILL==========")
+print("                                               ")
+
+home_section_bill = (Non_stick_Pan
++Pressure_cooker
++Dinner_Set
++Glass_Tumblers
++Stainless_Steel_Spoons
++Plastic_Storage_Containers
++Vegetable_Chopper
++Cleaning_Mop
++Dustbin
++Disbwashing_Brush
++Laundry_Basket
++Door_Mats
++Table_Mats
++Curtains
++lroning_Board
++Kitchen_Apron
++Serving_Trays
++Hangers
++Knife_Set
++Water_Bottle
+)
+print("====================")
+print("==========TOTAL HOME SECTION BILL==========")
+print("                                ")
+print("HOME SECTION BILL", home_section_bill)
+print("====================")
+
+print("                              ")
+print("==========COSMATICS SECTION==========")
+print("                                      ")
+Shampoo = int(input("enter the shampoo price="))
+Conditioner = int(input("enter the conditioner price="))
+Body_Soap = int(input("enter the body soap price="))
+Face_Wash = int(input("enter the face wash price="))
+Body_Lotion = int(input("enter the body lotion price="))
+Hand_Wash = int(input("enter the hand wash price="))
+Toothpaste = int(input("enter the toothpaste price="))
+Toothbrush = int(input("enter the toothbrush price="))
+Shaving_Cream = int(input("enter the shaving cream price="))
+Razor = int(input("enter the razor price="))
+Perfume_Deodorant = int(input("enter the perfume/deodorant price="))
+TalcumPowder = int(input("enter the talcumpowder price="))
+Moisturizing_Cream = int(input("enter the moisturizing cream price="))
+Nail_Polish = int(input("enter the nail polish price="))
+Lip_Balm = int(input("enter the lip balm price="))
+Cotton_Buds = int(input("enter the cotton buds price="))
+Wet_Wipes = int(input("enter the wet wipes price="))
+Hair_Oil = int(input("enter the hair oil price="))
+Face_Scrub = int(input("enter the face scrub price="))
+Hair_Gel = int(input("enter the hair gel price="))
+print("                                                ")
+
+print("===================")
+print("==========COSMATICS SECTION BILL==========")
+print("                                                  ")
+cosmatics_section_bill = (Shampoo
++Conditioner
++Body_Soap
++Face_Wash
++Body_Lotion
++Hand_Wash
++Toothpaste
++Toothbrush
++Shaving_Cream
++Razor
++Perfume_Deodorant
++TalcumPowder
++Moisturizing_Cream
++Nail_Polish
++Lip_Balm
++Cotton_Buds
++Wet_Wipes
++Hair_Oil
++Face_Scrub
++Hair_Gel
+)
+print("======================")
+print("==========TOTAL COSMATIC SECTION BILL")
+print("                             ")
+print("COSMATIC SECTION BILL", cosmatics_section_bill)
+print("====================")
+
+print("                            ")
+print("==========STATIONERY SECTION==========")
+print("                                        ")
+Notebooks = int(input("enter the notebooks price="))
+Ballpoint_pen = int(input("enter the ball pen price="))
+Pencils = int(input("enter the pencils price="))
+Erasers = int(input("enter the erasers price="))
+Sharpeners = int(input("enter the sharpners price="))
+Markers = int(input("enter the markers price="))
+Geometry_Box = int(input("enter the geometry box price="))
+School_Bag = int(input("enter the school bag price="))
+Drawing_Book = int(input("enter the drawing book price="))
+Water_Colors = int(input("enter the water colors price="))
+Glue_Stick = int(input("enter the glue stick price="))
+Stapler = int(input("enter the stapler price="))
+Paper_Clips = int(input("enter the paper clips price="))
+Files = int(input("enter the files price="))
+Scissors = int(input("enter the scissors price="))
+scale = int(input("enter the scale price="))
+Sketch_Pens = int(input("enter the sketch pens price="))
+Sticky_Notes = int(input("enter the sticky notes price="))
+Highlighter = int(input("enter the highlighter price="))
+Pencil_Case = int(input("enter the pencil case price="))
+print("                                  ")
+
+print("=====================")
+print("==========STATIONERY SECTION BILL==========")
+print("                                   ")
+stationery_section_bill = (Notebooks
++Pencils
++Erasers
++Sharpeners
++Markers
++Geometry_Box
++School_Bag
++Drawing_Book
++Water_Colors
++Glue_Stick
++Stapler
++Paper_Clips
++Files
++scale
++Sketch_Pens
++Sticky_Notes
++Highlighter
++Pencil_Case
++Ballpoint_pen
++Scissors
+)
+print("======================")
+print("==========TOTAL STATIONERY SECTION BILL==========")
+print("                                    ")
+print("STATIONREY SECTION BILL", stationery_section_bill)
+print("======================")
+
+print("                                   ")
+print("==========BABY COSMATICS==========")
+print("                                   ")
+Baby_Diapers = int(input("enter the baby diapers price="))
+Baby_Wipes = int(input("enter the baby wipes price="))
+Baby_Powder = int(input("enter the baby powder price="))
+Baby_Lotion = int(input("enter the baby lotion price="))
+Baby_Shampoo = int(input("enter the baby shampoo price="))
+Baby_Soap = int(input("enter the baby soap price="))
+Baby_Oil = int(input("enter the baby oil price="))
+Feeding_Bottle = int(input("enter the feeding bottle price="))
+Bottle_Brush = int(input("enter the bottle brush price="))
+Baby_Bibs = int(input("enter the baby bibs price="))
+Baby_Blanket = int(input("enter the baby blanmet price="))
+Baby_Towel = int(input("enter the baby towel price="))
+Baby_Teether = int(input("enter the baby teether price="))
+Baby_Rash_Cream = int(input("enter the baby rash cream="))
+Baby_Socks = int(input("enter the baby socks price="))
+Baby_Cotton_Buds = int(input("enter the baby cotton buds price="))
+Sipper_Cup = int(input("enter the sipper cup price="))
+Baby_Comb = int(input("enter the baby comb price="))
+Baby_Nail_Clipper = int(input("enter the baby nail clipper price="))
+Baby_Toy = int(input("enter the baby toy price="))
+print("                                              ")
+print("=====================")
+print("==========BABY COSMATICS BILL==========")
+print("                                       ")
+baby_cosmatics_bill = (Baby_Diapers
++Baby_Wipes
++Baby_Lotion
++Baby_Shampoo
++Baby_Soap
++Baby_Oil
++Feeding_Bottle
++Bottle_Brush
++Baby_Bibs
++Baby_Blanket
++Baby_Towel
++Baby_Teether
++Baby_Rash_Cream
++Baby_Socks
++Baby_Cotton_Buds
++Sipper_Cup
++Baby_Comb
++Baby_Nail_Clipper
++Baby_Toy
++Baby_Powder
+)
+print("====================")
+print("==========BABY COSMATICS SECTION BILL==========")
+print("                                               ")
+print("BABY COSMATICS SECTION BILL",baby_cosmatics_bill)
+print("                                        ")
+print("====================")
+
+print("                                         ")
+print("==========GARMENTS SECTION==========")
+print("                                     ")
+T_shirts = int(input("enter the tshirts price="))
+Jeans = int(input("enter the jeans price="))
+Formal_shirts = int(input("enter the formal shirts price="))
+Trousers = int(input("enter the trousers price="))
+Kids_frock = int(input("enter the kids frock price="))
+Baby_rompers = int(input("enter the baby rompers price="))
+Shalwar_kameez = int(input("enter the shalwar kameez price="))
+Jackets = int(input("enter the jackets price="))
+Sweaters = int(input("enter the sweaters price="))
+Socks =  int(input("enter the socks price="))
+Handkerchiefs = int(input("enter the handkerchiefs price="))
+Belts =  int(input("enter the belts price="))
+Caps =  int(input("enter the caps price="))
+Sneakers =  int(input("enter the sneakers price="))
+Slippers =  int(input("enter the slippers price="))
+Sandles =  int(input("enter the sandles price="))
+Ties =  int(input("enter the ties price="))
+Scarves =  int(input("enter the scarcves price="))
+Undergarments =  int(input("enter the undergarments price="))
+Nightwear =  int(input("enter the nightwear price="))
+print("                        ")
+print("==========GARMENTS SECTION BILL==========")
+print("                                 ")
+garments_section_bill = (T_shirts
++Jeans
++Formal_shirts
++Trousers
++Kids_frock
++Baby_rompers
++Shalwar_kameez
++Jackets
++Sweaters
++Socks
++Handkerchiefs
++Belts
++Caps
++Sneakers
++Slippers
++Sandles
++Ties
++Scarves
++Undergarments
++Nightwear
+)
+print("==========TOTAL GARMENTA SECTION BILL==========")
+print("                                         ")
+print("GARMENTS SECTION BILL",garments_section_bill)
+print("====================")
+
+print("                              ")
+print("==========TOYS SECTION==========")
+print("                                  ")
+Remote_control_car = int(input("enter the remote control car price="))
+Dolls = int(input("enter the dolls price="))
+Board_games = int(input("enter the board games price="))
+Soft_Toys = int(input("enter the soft toys price="))
+Cricket_Bat = int(input("enter the cricket bat price="))
+Football = int(input("enter the football price="))
+Badminton_Racket = int(input("enter the badminton racket price="))
+Drawing_Sets = int(input("enter the drawing sets price="))
+Action_Figures = int(input("enter the action figures price="))
+Toy_Kitchen_Set = int(input("enter the toy kitchen set price="))
+Educational_Toys = int(input("enter the educational toys price="))
+Stuffed_Animals = int(input("enter the stuffed animals price="))
+Skipping_Rope = int(input("enter the skipping rope price="))
+Toy_Train = int(input("enter the toy train price="))
+print("                         ")
+print("==========TOYS SECTION BILL==========")
+print("                              ")
+toys_section_bill = (Remote_control_car
++Dolls
++Board_games
++Soft_Toys
++Cricket_Bat
++Football
++Badminton_Racket
++Drawing_Sets
++Action_Figures
++Toy_Kitchen_Set
++Educational_Toys
++Stuffed_Animals
++Skipping_Rope
++Toy_Train
+)
+print("==========TOTAL TOY SECTION BILL==========")
+print("                                      ")
+print("TOY SECTION BILL", toys_section_bill)
+print("====================")
+
+print("                                     ")
+print("==========HARDEARE SECTION==========")
+print("                                     ")
+Screwdriver_Set = int(input("enter the screwdriver set price="))
+Hammer = int(input("enter the hammer price="))
+Pliers = int(input("enter the pliers price="))
+Measuring_Tape = int(input("enter the measuring tap price="))
+Duct_Tape_Electric_Tape = int(input("enter the duct tap and electric tap price="))
+Nails_and_Screws = int(input("enter the nails and screws price="))
+Wall_Hooks = int(input("enter the wall hooks price="))
+Flashlight = int(input("enter the flashlight price="))
+Batteries = int(input("enter the batteries price="))
+Glue_Gun = int(input("enter the glue gun price="))
+Door_Locks = int(input("enter the door locks price="))
+Paint_Brushes = int(input("enter the paint brushes price="))
+Sandpaper =int(input("enter the sandpaper price="))
+Spanner = int(input("enter the spanner price="))
+Extension_Cord =int(input("enter the extension cord price="))
+Fuses = int(input("enter the fuses price="))
+Furniture_Pads =int(input("enter furniture pads train price="))
+Tool_Box = int(input("enter the tool box price="))
+Door_Stoppers =int(input("enter the door stoppers price="))
+Key_Chains = int(input("enter the key chains price="))
+print("                       ")
+print("===========HARDEARE SECTION BILL==========")
+print("                                  ")
+hardware_section_bill = (Screwdriver_Set
++Hammer
++Pliers
++Measuring_Tape
++Duct_Tape_Electric_Tape
++Nails_and_Screws
++Wall_Hooks
++Flashlight
++Batteries
++Glue_Gun
++Door_Locks
++Paint_Brushes
++Sandpaper
++Spanner
++Extension_Cord
++Fuses
++Furniture_Pads
++Tool_Box
++Door_Stoppers
++Key_Chains
+)
+print("==========TOTAL HARDWARE SECTION BILL==========")
+print("                                               ")
+print("HARDWARE SECTION BILL", hardware_section_bill)
+print("====================")
+print("                                ")
+grande_bill = (food_section_bills
++electric_section_bill
++home_section_bill
++cosmatics_section_bill
++stationery_section_bill
++baby_cosmatics_bill
++garments_section_bill
++toys_section_bill
++hardware_section_bill
+)
+customer_name = input("pleas enter your name")
+phone_number = input("pleas enter your phone number")
+address = input("pleas enter your home address")
+print("=========================================")
+print("     MEGA MART     ")
+print("=========================================")
+print("                                       ")
+print("CUSTOMER NAME :",customer_name)
+print("PHONE NUMBER :",phone_number)
+print("ADDRESS :",address)
+print("                   ")
+print("")
+print("FOOD SECTION      =",food_section_bills)
+print("ELECTRIC SECTION  =",electric_section_bill)
+print("HOME SECTION      =",home_section_bill)
+print("COSMATIC SECTION  =",cosmatics_section_bill)
+print("STATIONERY SECTION=",stationery_section_bill)
+print("BABY SECTION      =",baby_cosmatics_bill)
+print("GARMENTS SECTION  =",garments_section_bill)
+print("TOYS SECTION      =",toys_section_bill)
+print("HAEDWARE SECTION  =",hardware_section_bill)
+print("                                    ")
+print("_______________")
+print("FINAL BILL =",grande_bill)
+print("================================")
+print("THANK YOU FOR SHOPPING!")
+
